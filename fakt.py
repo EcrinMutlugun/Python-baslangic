334682d08cdf80af8f8a48e775035975e4bf2a8d
EcrinMutlugun <ecmutlugunn@gmail.com>
2026-04-29 21:10:07 +0300

    Update fakt.py

--- fakt.py
+++ fakt.py
@@ -1,44 +1,27 @@
-# For Döngüsü İle Faktöriyel Hesabı:
-# while True:
-#     try:
-#         sayi = int(input("Lütfen bir sayı giriniz: "))
-#     except ValueError:
-#         print("Lütfen yalnızca sayı girin! ")
-#         continue
+def faktoriyel_for(sayi):
+    fakt =1
+    i=1
+    for i in range(1,sayi+1):
+        fakt *= i
+        i += 1
+    return fakt
 
-#     fakt = 1
-#     i = 1
-#     for i in range(1,sayi+1):
-#         fakt *= i
-#         i += 1
-#     print(f"{sayi}! = ",fakt)
 
-# While Döngüsü İle Faktöriyel Hesabı:
-# while True:
-#     try:
-#         sayi = int(input("Lütfen bir sayı girin: "))
-#     except ValueError:
-#         print("Lütfen yalnızca sayı girin! ")
-#         continue
+def faktoriyel_while(sayi):
+    i = 2
+    fakt =1
+    while i <= sayi:
+        fakt *= i
+        i+=1
+    return fakt
 
-#     i = 2
-#     fakt = 1
-#     while i <= sayi:
-#         fakt *= i
-#         i += 1
-#     print(f"{sayi}! = {fakt}")
-
-# Fonksiyon İle Faktöriyel Hesabı:
-def fakt(n):
-    sonuc = 1
-    for i in range(1,n+1):
-        sonuc *= i
-    return sonuc
 
 while True:
-    try:
-        sayi = int(input("Lütfen bir sayı giriniz: "))
-    except ValueError:
-        print("Lütfen yalnızca sayı girin! ")
-        continue
-    print(f"{sayi}! = {fakt(sayi)}")
+        try:
+            sayi = int(input("Lütfen bir sayı giriniz: "))
+        except ValueError:
+            print("Lütfen yalnızca sayı girin! ")
+            continue
+
+        print(f"For döngüsü: ", faktoriyel_for(sayi))
+        print(f"While döngüsü: ", faktoriyel_while(sayi))
